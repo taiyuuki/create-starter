b0ca80bf1454a765a5235c4dd215052c2a63ef7b
taiyuuki <taiyuuki@qq.com>
2022-11-10 18:28:28 +0800

    fix: Template -> Vue -> Quasar -> stylelint

--- templates/vue/quasar/base/_.stylelintrc.js
+++ templates/vue/quasar/base/_.stylelintrc.js
@@ -2,10 +2,11 @@ module.exports = {
   extends: [
     'stylelint-config-standard',
     'stylelint-config-standard-scss',
-    'stylelint-config-rational-order'
+    'stylelint-config-recommended-vue/scss',
+    'stylelint-config-rational-order',
   ],
   rules: {
     'selector-class-pattern': '.*',
-    'max-line-length': [120, { ignorePattern: /content/ }]
-  }
+    'max-line-length': [120, { ignorePattern: /content/ }],
+  },
 }
