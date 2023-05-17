41ea828e8907b154d38771b9a410bdee836d55b0
taiyuuki <taiyuuki@qq.com>
2023-05-17 09:50:49 +0800

    fix: update

--- scripts/update.ts
+++ scripts/update.ts
@@ -5,11 +5,16 @@ import { versions } from '../src/versions'
 const names = Object.keys(versions) as (keyof typeof versions)[]
 async function update() {
     const result: Record<string, string> = {}
+    const reg = /[a-zA-Z]/
     for await (const name of names) {
         const data = await pacote.packument(name, { registry: 'https://registry.npm.taobao.org' })
         if (data) {
             const versions = Object.keys(data.versions)
-            result[name] = '^' + versions[versions.length - 1]
+            let i = versions.length - 1
+            result[name] = '^' + versions[i]
+            while (reg.test(result[name])) {
+                result[name] = '^' + versions[--i]
+            }
         }
     }
     let code = 'export const versions = '
@@ -21,7 +26,3 @@ async function update() {
 }
 
 update()
-// for await (const name of names) {
-//     const data = await pacote.packument(name)
-//     console.log(data.versions)
-// }
